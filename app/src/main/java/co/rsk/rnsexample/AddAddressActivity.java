4cf097370e2d79b4d9e32fadabd10e91e284c4fb
Joaquin Diaz Trepat <joaquin@rsk.co>
2019-04-22 12:49:31 -0300

    Example app with fab button

---
+++ app/src/main/java/co/rsk/rnsexample/AddAddressActivity.java
@@ -0,0 +1,58 @@
+package co.rsk.rnsexample;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import co.rsk.rnssdk.BuildConfig;
+import co.rsk.rnssdk.RnsResolver;
+
+public class AddAddressActivity extends AppCompatActivity {
+
+    RnsResolver resolver = new RnsResolver(BuildConfig.NODE, BuildConfig.RESOLVER_ADDRESS);
+
+    private final static String NO_RESPONSE = "0x0000000000000000000000000000000000000000";
+
+    private class ResolveNameAddress extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String result = resolver.getAddress(params[0]);
+                if (!result.equals(NO_RESPONSE)) {
+                    return params[0]+":"+result;
+                }
+            } catch (Exception e) {
+                return null;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent();
+            intent.putExtra(ADDRESS_DATA, result);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+
+    public final static String ADDRESS_DATA = "ADDRESS_DATA";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_address);
+
+        EditText address = findViewById(R.id.addressEditTextID);
+        Button addbutton = findViewById(R.id.addAddressID);
+        addbutton.setOnClickListener(v -> {
+            if (address.getText().length() > 0) {
+                ResolveNameAddress resolve = new ResolveNameAddress();
+                resolve.execute(address.getText().toString());
+            }
+        });
+    }
+}
--- app/src/main/java/co/rsk/rnsexample/MainActivity.java
+++ app/src/main/java/co/rsk/rnsexample/MainActivity.java
@@ -1,7 +1,11 @@
 package co.rsk.rnsexample;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -10,36 +14,55 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    FloatingActionButton floatingActionButton;
 
     List<String> nameArray = new ArrayList<>();
 
     {
-            nameArray.add("Octopus");
-            nameArray.add("Pig");
-            nameArray.add("Sheep");
-            nameArray.add("Rabbit");
-            nameArray.add("Snake");
-            nameArray.add("Spider");
+            nameArray.add("Dummy");
     }
 
     List<String> infoArray = new ArrayList<>();
 
     {
-        infoArray.add("8 tentacled monster");
-        infoArray.add("Delicious in rolls");
-        infoArray.add("Great for jumpers");
-        infoArray.add("Nice in a stew");
-        infoArray.add("Great for shoes");
-        infoArray.add("Scary.");
+        infoArray.add("0xAF415eAAD8E55");
     }
 
+    private static final String LOG = "main activity";
+
+    CustomListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CustomListAdapter adapter = new CustomListAdapter(this, nameArray, infoArray);
+        adapter = new CustomListAdapter(this, nameArray, infoArray);
         listView = findViewById(R.id.domainsListID);
         listView.setAdapter(adapter);
+
+    }
+    static final int ADD_ADDRESS_REQUEST = 0;
+
+    public void addAddress(View view) {
+        Log.d(LOG, "Button clicked!");
+        startActivityForResult(new Intent(this, AddAddressActivity.class), ADD_ADDRESS_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == ADD_ADDRESS_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                System.out.println(data.getExtras().getString(AddAddressActivity.ADDRESS_DATA));
+                String res = data.getExtras().getString(AddAddressActivity.ADDRESS_DATA);
+                if (res != null) {
+                    String[] parsed = res.split(":");
+                    if (parsed.length > 1) {
+                        nameArray.add(parsed[0]);
+                        infoArray.add(parsed[1]);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        }
     }
 }
